a0a51180c857fc6dc59e21ecfb81064155110b20
Egor Dubovik <egorka54896@gmail.com>
2023-02-13 20:12:35 +0300

    feet: add errorHandler(middleware)

---
+++ middleware/ErrorHandlingMiddleware.js
@@ -0,0 +1,10 @@
+import ApiError from "../error/ApiError.js";
+
+const errorHandler = (err, req, res, next) => {
+  if (err instanceof ApiError) {
+    return res.status(err.status).json({ message: err.message });
+  }
+  return res.status(5000).json({ message: "Unexpected error" });
+};
+
+export default errorHandler;
