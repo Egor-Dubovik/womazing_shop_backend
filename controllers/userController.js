890ccbd3fc16900b3b03baa52017fbc1bc63847b
Egor Dubovik <egorka54896@gmail.com>
2023-02-14 12:53:19 +0300

    feet: add methods (registration, login) for UserController and create function generateJwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,46 @@
 import ApiError from "../error/ApiError.js";
+import models from "../models/models.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 
 class UserController {
-  async registration(req, res) {}
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+    if (!email && !password) return next(ApiError.badRequest("Некорректный email или password"));
 
-  async login(req, res) {}
+    const candidate = await models.User.findOne({ where: { email } });
+    if (candidate) return next(ApiError.badRequest("Пользователь с таким email уже существует"));
+
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await models.User.create({ email, password: hashPassword, role });
+    const basket = await models.Basket.create({ userId: user.id });
+    const token = generateJwt(user.id, user.email, user.role);
+
+    return res.json({ token });
+  }
+
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await models.User.findOne({ where: { email } });
+    if (!user) return next(ApiError.internal("Пользователь с таким email не найден"));
+
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) return next(ApiError.internal("Указан не верный пароль"));
+
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
 
   async check(req, res, next) {
     const { id } = req.query;
     if (!id) {
       return next(ApiError.badRequest("there isn't id"));
-      // response.end();
     }
     res.json(id);
   }
